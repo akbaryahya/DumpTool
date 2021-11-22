d1f8ac4cce47a129915f6a1374380a27cc8c3754
Akbar Yahya <11765082+akbaryahya@users.noreply.github.com>
2021-11-23 03:17:27 +0800

    better info

--- install.sh
+++ install.sh
@@ -11,20 +11,37 @@ echo "======================="
 echo "Clone Tool"
 echo "======================="
 mkrdir Tool && rm -r *
+echo "Install Masscan"
 git clone https://github.com/robertdavidgraham/masscan && cd masscan && make && make install && cd ..
+echo "Install Asleep Scanner"
 git clone https://github.com/d34db33f-1007/asleep_scanner && cd asleep_scanner && pip3 install -r requirements.txt & cd ..
+echo "Install Coolab"
 git clone https://github.com/songlinhou/coolab && cd coolab && pip install . && cd ..
-curl https://rclone.org/install.sh | sudo bash -s beta
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.tgz && unzip ngrok.zip
-cd ..
+# cd ..
 echo "======================="
-echo "Start Setup?"
+echo "Install Ngrok"
 echo "======================="
 read -p "Ngrok Token: " NROK
 if [ -z "$NROK" ]
 then 
- echo "Ngrok Skip"
+ echo "RDP skip"
 else
- echo "Ngrok Login..."
+ echo "Ngrok Install"
+ wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip && unzip ngrok.zip
+ echo "Ngrok: Set Token"
  ./ngrok/ngrok authtoken $NROK
+ echo "Ngrok: Set Port 3389"
+ nohup ./ngrok/ngrok tcp 3389 &>/dev/null &
+ sudo firefox xrdp xfce4 xfce4-terminal
+fi
+echo "======================="
+echo "Install Rclone"
+echo "======================="
+read -p "Rclone config file: " NROK
+if [ -z "$NROK" ]
+then 
+ echo "Rclone skip"
+else
+ echo "Rclone todo"
+ curl https://rclone.org/install.sh | sudo bash -s beta
 fi
